9b82e05e7e4339691830afc30d011347f6d2efee
qwer1227 <113608501+qwer1227@users.noreply.github.com>
2024-07-22 16:44:20 +0900

    [Silver IV] Title: 설탕 배달, Time: 224 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n == 4 || n == 7){
+            System.out.println(-1);
+        }
+        else if(n % 5 == 0){
+            System.out.println(n/5);
+        }
+        else if(n % 5 == 1){
+            System.out.println(n/5+1);
+        }
+        else if(n % 5 == 2){
+            System.out.println(n/5+2);
+        }
+        else if(n % 5 == 3){
+            System.out.println(n/5+1);
+        }
+        else if(n % 5 == 4){
+            System.out.println(n/5+2);
+        }
+
+    }
+}
