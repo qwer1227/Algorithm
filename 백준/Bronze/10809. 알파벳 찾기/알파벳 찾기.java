50d4b65ee9195a1333be6ee56fae2844f90b633e
CoMot4 <113608501+qwer1227@users.noreply.github.com>
2025-03-26 17:11:45 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 184 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine(); // baekjoon
+
+        String[] arrays = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+        for(int i = 0; i < arrays.length; i++){
+            int index = input.indexOf(arrays[i]);
+
+            System.out.println(index);
+        }
+
+
+    }
+}
