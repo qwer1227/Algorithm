7fa4e79787937af649542e9b8ff429ff8d68873c
CoMot4 <113608501+qwer1227@users.noreply.github.com>
2025-03-27 14:24:22 +0900

    [Silver IV] Title: 스택, Time: 352 ms, Memory: 20196 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Stack<Integer> arr = new Stack<>();
+
+        int size = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < size; i++) {
+            String s = br.readLine();
+            String[] num = s.split(" ");
+
+            if (s.contains("push")) {
+                arr.push(Integer.parseInt(num[1]));
+            } else if (s.contains("pop")) {
+                if (arr.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(arr.pop() + "\n");
+                }
+            } else if (s.contains("size")) {
+                bw.write(arr.size() + "\n");
+            } else if (s.contains("top")) {
+                if (arr.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(arr.peek() + "\n");
+                }
+            } else if (s.contains("empty")) {
+                bw.write(arr.isEmpty() ? "1\n" : "0\n");
+            }
+
+            bw.flush();
+        }
+        bw.close();
+
+    }
+}
