6439c8f8f2852331981182b2eab8038f19f87578
CoMot4 <113608501+qwer1227@users.noreply.github.com>
2025-07-15 17:31:41 +0900

    [Silver III] Title: 피보나치 함수, Time: 192 ms, Memory: 18344 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -6,22 +6,25 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        int[][] temp = new int[41][2];
-        temp[0][0] = 1;
-        temp[0][1] = 0;
-
-        temp[1][0] = 0;
-        temp[1][1] = 1;
-
-        for (int i = 2; i <= 40; i++) { //값을 할당하는 반복문 2부터 시작
-            temp[i][0] = temp[i - 1][0] + temp[i - 2][0]; // [2][0] >> n이 2일때 0이 몇번 나왔는지 구하는 
-            temp[i][1] = temp[i - 1][1] + temp[i - 2][1]; // [2][1] >> n이 2일때 1이 몇번 나왔는지 구하는
-        }
+        int[] temp = new int[41];
+        temp[0] = 1;
+        temp[1] = 1;
+        temp[2] = 2;
 
         for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
-            System.out.println(temp[n][0] + " " + temp[n][1]); //0을 호출, 1을 호출한횟수
+            if (n == 0) {
+                System.out.println("1 0");
+            } else if (n == 1) {
+                System.out.println("0 1");
+            } else if (n == 2) {
+                System.out.println("1 1");
+            } else {
+                for (int j = 3; j < n; j++) {
+                    temp[j] = temp[j - 1] + temp[j - 2];
+                }
+                System.out.println(temp[n - 2] + " " + temp[n - 1]);
+            }
         }
-
     }
 }
